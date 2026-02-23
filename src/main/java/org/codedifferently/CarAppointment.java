947ceec998bf5b3850c1a83ea9464909b0b08822
whblue261 <whblue261@gmail.com>
2026-02-22 22:41:19 -0500

    update car appointment and shop scheduler

--- src/main/java/org/codedifferently/CarAppointment.java
+++ src/main/java/org/codedifferently/CarAppointment.java
@@ -1,4 +1,29 @@
 package org.codedifferently;
 
 public class CarAppointment {
-}
+         // The customer who booked
+        private CarPatient customer;
+         // Type of repair
+        private String serviceType;
+         // Index of time slot in the schedule
+        private int slotIndex;
+        //The constructor w each contract
+        public CarAppointment(CarPatient customer, String serviceType, int slotIndex) {
+            this.customer = customer;
+            this.serviceType = serviceType;
+            this.slotIndex = slotIndex;
+        }
+
+        public CarPatient getCustomer() {
+            return customer;
+        }
+
+        public String getServiceType() {
+            return serviceType;
+        }
+
+        public int getSlotIndex() {
+            return slotIndex;
+        }
+    }
+
