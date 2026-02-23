5fb9dc354c78a4255da5b09b992f62439dac78cd
BennettDeveloper <bennettdeveloper@gmail.com>
2026-02-22 23:32:04 -0500

    fix: implementing new time slot class

---
+++ src/main/java/org/codedifferently/data/TimeSlot.java
@@ -0,0 +1,26 @@
+package org.codedifferently.data;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Duration;
+
+public class TimeSlot {
+    private final LocalDateTime start;
+    private final LocalDateTime end;
+
+    public TimeSlot(LocalDateTime start, LocalDateTime end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public Duration getDuration() {
+        return Duration.between(start, end);
+    }
+}
